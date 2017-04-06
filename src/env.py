2270e94cd6ec868557844a413e397951e6338157
Phillip <kip.phil@gmail.com>
2017-04-06 18:40:14 -0400

    initial files

---
+++ src/env.py
@@ -0,0 +1,20 @@
+from gym import Env
+from gym.envs.registration
+import numpy as np
+
+
+class MagnetsEnv(Env):
+    def __init__(self):
+        pass
+
+    def _reset(self):
+        pass
+
+    def _step(self):
+        pass
+
+    def _render(self):
+        pass
+
+    def _seed(self):
+        pass
