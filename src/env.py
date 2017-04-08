659e2859f9f77fc68f06b1ac4318f88f9ee37483
Sidhanth Mohanty <sidhanth@cmu.edu>
2017-04-07 21:44:57 -0400

    state.py is correct and tested. wrote some part of env.py

--- src/env.py
+++ src/env.py
@@ -4,17 +4,42 @@ import numpy as np
 
 
 class MagnetsEnv(Env):
-    def __init__(self):
-        pass
+    def __init__(self, G_const=1.0, acceleration=1.0, time_step=0.01,
+                time_limit=10, speed_limit=1.0, seed=29, boundary_less = -1,
+                boundary_greater = 1, num_agents = 3):
+        ''' constants '''
+        self.G_const = G_const
+        self.acceleration = acceleration
+        self.time_step = time_step
+        self.time_limit = time_limit
+        self.speed_limit = speed_limit
+        self.seed = seed
+        self.boundary_less = boundary_less
+        self.boundary_greater = boundary_greater
+        self.num_agents = num_agents
+
+        ''' variables that change with time '''
+        self.num_steps = 0
+        self.state = State(num_agents, seed)
 
     def _reset(self):
-        pass
+        self.__init__()
 
     def _step(self):
-        pass
+        ''' evolve the state  '''
+        self.state.target_pos += (self.state.target_vel * time_step)
+        self.num_steps += 1
+        total_acc = np.zeros(2)
+
+        for i in range(self.num_agents):
+            diff_i = self.state.target_pos - self.state.agent_states[i].pos
+            dist_square = (diff[0] * diff[0]) + (diff[1] * diff[1])
+            total_acc += (self.G_const / dist_square) * (diff_i / sqrt(dist_square))
+
+        self.state.target_vel += (total_acc * self.time_step)
+
+        ''' update velocities based on acceleration '''
+
 
     def _render(self):
-        pass
-
-    def _seed(self):
         pass
---
+++ src/state.py
@@ -0,0 +1,46 @@
+import math
+import numpy as np
+
+class ObjState:
+    def __init__(self, position, velocity):
+        self.pos = position
+        self.vel = velocity
+
+class State:
+    def __init__(self, num_agents, seed=29):
+        target_pos = np.zeros(2)
+        target_vel = np.zeros(2)
+        self.target_state = ObjState(target_pos, target_vel)
+
+        self.num_agents = num_agents
+        np.random.seed(seed)
+        rads = math.sqrt(2) * np.random.rand(num_agents)
+        angles = (2.0 * math.pi / num_agents) * np.random.rand(num_agents)
+        self.agent_states = []
+        
+        print("Type of angles is: ", type(angles))
+
+        for i in range(num_agents):
+            angles[i] += (i * 2.0 * math.pi) / num_agents
+            pos_x = rads[i] * math.cos(angles[i])
+            pos_y = rads[i] * math.sin(angles[i])
+            agent_pos = np.asarray([pos_x, pos_y])
+            agent_vel = np.zeros(2)
+            agent_state = ObjState(agent_pos, agent_vel)
+            self.agent_states.append(agent_state)
+
+    def print_state(self):
+        print("Target position:", self.target_state.pos)
+        print("Target velocity:", self.target_state.vel)
+
+        print("Agent info: ")
+        for i in range(self.num_agents):
+            print("Agent", i, "position:", self.agent_states[i].pos)
+            print("Agent", i, "velocity:", self.agent_states[i].vel)
+
+def main():
+    test_state = State(3, 29)
+    test_state.print_state()
+
+if __name__ == '__main__':
+    main()
