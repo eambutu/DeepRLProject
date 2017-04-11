940f6a80b8ee3c4a59ef3afb51ee46ddde8c7f71
Kumail Jaffer <mkumailjaffer@gmail.com>
2017-04-11 15:10:05 -0400

    save/restore weights for training that actually works

--- src/rl/dqn.py
+++ src/rl/dqn.py
@@ -111,13 +111,15 @@ class DQNAgent:
 
         # Tensorflow graph information
         self.sess = None
+
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
         with tf.variable_scope("train_network"):
             self.train_s = tf.placeholder(tf.float32, (None, ) + env.observation_space.shape)
             self.train_a = tf.placeholder(tf.float32, (None, env.action_space.n))
             self.train_y = tf.placeholder(tf.float32, (None,))
             self.train_q = self.q_network(self.train_s, env.action_space.n)
             cost = self.loss(tf.reduce_sum((self.train_q * self.train_a), axis=-1) - self.train_y)
-            self.optimize_op = self.optimizer.minimize(cost)
+            self.optimize_op = self.optimizer.minimize(cost, global_step=self.global_step)
             train_vars = tf.get_collection(tf.GraphKeys.MODEL_VARIABLES, scope="train_network")
             avg_q = tf.reduce_mean(self.train_q)
 
@@ -148,6 +150,10 @@ class DQNAgent:
             self.train_a: a,
             self.train_y: y,
         })
+        return self._global_step()
+
+    def _global_step(self):
+        return tf.train.global_step(self.sess, self.global_step)
 
     def _calc_metrics(self, s, a, y):
         return self.sess.run(self.metrics, feed_dict={
@@ -159,7 +165,7 @@ class DQNAgent:
     def _sync_target_network(self):
         self.sess.run(self.target_update_op)
 
-    def train(self, policy, max_samples):
+    def train(self, policy, max_updates):
         """
         Train the DQN Agent
 
@@ -183,15 +189,16 @@ class DQNAgent:
             self.sess = sess
             checkpoint = tf.train.latest_checkpoint(self.save_dir)
             if (checkpoint is not None):
+                print ("restoring from checkpoint file %s" % checkpoint)
                 saver.restore(sess, checkpoint)
-
-            sess.run(init_op)
+            else:
+                sess.run(init_op)
 
             n_samples = 0
-            n_updates = 0
+            n_updates = self._global_step()
             n_episodes = 0
             episode_reward = 0  # keep track of this to see if we are learning
-            while (n_samples <= max_samples + self.num_burn_in):
+            while (n_updates <= max_updates):
                 state = self.env.reset()
                 is_terminal = False
                 while (not is_terminal):
@@ -212,22 +219,21 @@ class DQNAgent:
                             (b_s, b_a, b_ns, b_r, b_t) = \
                                 process_samples(batch, self.env.action_space.n)
                             b_y = self._calc_q_update(b_ns, b_r, b_t)
-                            self._train_step(b_s, b_a, b_y)
+                            n_updates = self._train_step(b_s, b_a, b_y)
                             self.latest_metrics = self._calc_metrics(b_s, b_a, b_y)
                             if (n_updates % self.target_update_interval == 0):
                                 self._sync_target_network()
-                            n_updates += 1
 
-                    if (n_samples % self.report_interval == 0):
-                        avg_reward = episode_reward/n_episodes if n_episodes != 0 else 0
-                        print("reward/episode since last report: %f" % avg_reward)
-                        print(self.metric_str % tuple(self.latest_metrics))
-                        print("%d experiences sampled" % n_samples)
-                        print("")
-                        saver.save(sess, "%s/model" % self.save_dir,
-                                   global_step=n_samples-self.num_burn_in)
-                        n_episodes = 0
-                        episode_reward = 0
+                        if (n_updates % self.report_interval == 0):
+                            avg_reward = episode_reward/n_episodes if n_episodes != 0 else 0
+                            print("reward/episode since last report: %f" % avg_reward)
+                            print(self.metric_str % tuple(self.latest_metrics))
+                            print("%d experiences sampled" % n_samples)
+                            print("%d updates performed" % n_updates)
+                            print("")
+                            saver.save(sess, "%s/model" % self.save_dir, global_step=n_updates)
+                            n_episodes = 0
+                            episode_reward = 0
 
                     n_samples += 1
                     state = next_state
