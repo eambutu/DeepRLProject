7c2d49c717b387e120846c1d421761df4f5f076d
Kumail Jaffer <mkumailjaffer@gmail.com>
2017-04-15 19:23:18 -0400

    multi-agent general model wip

--- src/dqn_agent.py
+++ src/dqn_agent.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import argparse
+from functools import partial
 
 import gym
 from gym.wrappers import Monitor
+import tensorflow as tf
 import tflearn
 
 import rl.env # noqa : F401
@@ -17,11 +19,45 @@ EPSILON_START = 0.5
 EPSILON_END = 0.01
 EPSILON_STEPS = 10000
 
+VIDEO_INTERVAL = 10000
 
-def q_model(x, n):
+
+def single_agent_model(x, n_actions):
     h1 = tflearn.fully_connected(x, 20, activation='relu')
-    out = tflearn.fully_connected(h1, n)
+    out = tflearn.fully_connected(h1, n_actions)
     return out
+
+
+def sequential_model(x, n_agents, n_actions):
+    qs = [single_agent_model(x, n_actions, 1)]
+    smaxes = [tflearn.activations.softmax(qs[-1])]
+
+    for i in range(1, n_agents):
+        smaxes.append(x)
+        x_i = tf.concat(smaxes, axis=1)
+        smaxes.remove(x)
+
+        qs.append(single_agent_model(x_i, n_actions))
+        smaxes.append(tflearn.activations.softmax(qs[-1]))
+
+    out = tf.stack(qs, axis=1)
+    return out
+
+
+def parallel_model(x, n_agents, n_actions):
+    qs = []
+    for _ in range(n_agents):
+        qs.append(single_agent_model(x, n_actions))
+    out = tf.stack(qs, axis=1)
+    return out
+
+
+def no_video_schedule(i):
+    return False
+
+
+def regular_video_schedule(n, i):
+    return i % n == 0
 
 
 if __name__ == '__main__':
@@ -33,8 +69,11 @@ if __name__ == '__main__':
     parser.add_argument('--train', default=False, action='store_true', help='train model')
     parser.add_argument('--monitor', default=False, action='store_true',
                         help='use the gym monitor wrapper to monitor progress')
+    parser.add_argument('--video', default=False, action='store_true',
+                        help='when using monitor, also store video')
     parser.add_argument('--iterations', default=None, type=int,
                         help='when evaluating, use the model trained for this many iterations')
+    parser.add_argument('--model', type=str, default='parallel', choices=['parallel', 'sequential'])
 
     args = parser.parse_args()
 
@@ -43,9 +82,23 @@ if __name__ == '__main__':
         parser.print_help()
         exit(1)
 
+    if (args.model == 'parallel'):
+        q_model = parallel_model
+    elif (args.model == 'sequential'):
+        q_model = sequential_model
+    else:
+        print("unrecognized model %s" % args.model)
+        parser.print_help()
+        exit(1)
+
+    if args.video:
+        video_callable = partial(regular_video_schedule, VIDEO_INTERVAL)
+    else:
+        video_callable = no_video_schedule
+
     env = gym.make(args.env)
     if (args.monitor):
-        env = Monitor(env, "%s/monitor" % args.name)
+        env = Monitor(env, "%s/monitor" % args.name, video_callable=video_callable)
 
     agent = DQNAgent(env, q_model, model_name=args.name)
     train_policy = LinearDecayGreedyEpsilonPolicy(EPSILON_START, EPSILON_END, EPSILON_STEPS)
